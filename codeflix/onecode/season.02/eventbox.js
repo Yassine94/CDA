32f442bc2c6eedaad5e651776f1e3d22b1d38156
Student <Student@B15-16.local>
2018-10-30 14:21:14 +0100

    addin season02

---
+++ codeflix/onecode/season.02/eventbox.js
@@ -0,0 +1,28 @@
+const EventEmitter = require('events').EventEmitter;
+const myEvent = new EventEmitter();
+
+function  empty(){
+    myEvent.on('hi', () => {
+      console.log('Ch0ooooooper!');
+    });
+    myEvent.emit('hi');
+  }
+
+  function withArgs(arg) {
+    myEvent.on('newFellow', () => {
+      if(typeof(arg) == 'string' ){
+
+        console.log(`Here come's a new pirate ->> ${arg}`);
+
+      }
+      else {
+        for(let item in arg) {
+          console.log(`Here come's a new pirate ->> ${arg[item]}`);
+        
+      }
+    });
+    myEvent.emit('newFellow');
+
+  }
+
+module.exports = { empty,withArgs }
