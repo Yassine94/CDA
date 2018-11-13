f695a0eab1de00fa0a73c29b462fe1a350c17c31
Student <Student@B15-16.local>
2018-11-13 08:23:17 +0100

    adding new files

--- codeflix/onecode/season.04/ep03.js
+++ codeflix/onecode/season.04/ep03.js
@@ -1,29 +1,39 @@
-const http = require('http');
-const url = require('url');
-const hostname = "127.0.0.1";
-var fs = require('fs');
 
+const http = require ('http');
+const https = require ('https');
+const url = require ('url')
+let urlJson = 'https://www.reddit.com/r/perfectloops.json'
+​
 let port = process.argv[2];
-
-if (port == undefined ) {
-  const port = 1335
-   console.log('Default port 1335 has been used');
+​
+if (port == undefined){
+​
+port = 4242
+console.log(`Server is running at port ${port}`);
+​
+http.createServer(function (req, res) {
+​
+ const request = https.get(`https://www.reddit.com/r/perfectloops.json`, (response) => {
+ let body = "";
+ response.on('data', data => {
+   //console.log('Reading Data');
+   body += data.toString();
+ });
+ response.on('end', () => {
+   let json = JSON.parse(body);
+   let string = ""
+   for(let i = 1; i < 11; i++) {
+      let gifLink = json.data.children[i].data.url;
+      // on transforme les gifv en gif pour les afficher correctemennt
+      gifLink = gifLink.replace('.gifv','.gif')
+      string += `<div>
+                  <img src = "${gifUrl}" >
+                 </div>`
+      res.writeHeader(200, {"Content-Type": "text/html"});
+   }
+   res.write(string);
+   res.end()
+  });
+ });
+}).listen(port);
 }
-
-const server = http.createServer(function(req, res) {
-      const q = url.parse(req.url,true);
-      let query = q.query;
-      let value = Object.values(query)
-      let userValue = value[0]
-      let data = JSON.stringify(userValue);
-      fs.writeFileSync("history.json", data);
-
-      console.log(userValue);
-
- res.end('done');
-
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at port : 1335`);
-});
